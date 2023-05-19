ece2fa13aee67a5cc20b0d5b0370c220ab416754
Lakshmi Sruthi <lakshmisruthi.kandikonda@factset.com>
2023-05-19 13:15:00 +0530

    commit2

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Organizational Tree</h1>
+    <TreeStructure :data="treeData" />
+  </div>
+</template>
+
+<script>
+import TreeStructure from "./components/TreeStructure.vue";
+
+export default {
+  name: "App",
+  components: {
+    TreeStructure
+  },
+  data() {
+    return {
+      treeData: [
+        {
+          id: "parent",
+          label: "Parent",
+          children: [
+            { id: "a", label: "Child A", children: [] },
+            { id: "b", label: "Child B", children: [] },
+            { id: "c", label: "Child C", children: [] }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/components/TreeStructure.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in treeData" :key="item.id">
+        {{ item.label }}
+        <TreeStructure :data="item.children" v-if="item.children && item.children.length > 0" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TreeStructure",
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      treeData: []
+    };
+  },
+  mounted() {
+    this.treeData = this.data;
+  }
+};
+</script>
